8b3512d5c18635744733371c4d4fd4aa1d699352
Qdesai <qaasimdesai786@gmail.com>
2022-06-03 19:09:05 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ $(function() {
 
     $("a#prospect").on('click', function(){
         xhr.open("GET", "prospect.html");
+        xhr.onload = function () { 
+            if (xhr.status === 200){
+                
+                d.innerHTML = xhr.responseText;}
+            
+            };
     });
 
     $("a#convert").on('click', function(){
@@ -20,15 +26,7 @@ $(function() {
 let d = document.getElementById("content");
 document.getElementById("content").style.display = "block";
 
-var xhr = new XMLHttpRequest();
-        
-        
+var xhr = new XMLHttpRequest();        
 let el = document.getElementById("content");
 
-xhr.onload = function () { 
-if (xhr.status === 200){
-    
-    d.innerHTML = xhr.responseText;}
-
-};
 xhr.send();
