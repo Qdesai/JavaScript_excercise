fa56fbcfe35bf4758876c74c8b4575d3fdaa6fae
Qdesai <qaasimdesai786@gmail.com>
2022-06-03 01:07:36 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 // WEB303 Assignment 2
-// Your Name Here
+// Mohammed Qaasim Desai
 
+
+$(function() {
+
+    $("a#prospect").on('click', function(){
+
+    });
+
+    $("a#convert").on('click', function(){
+        console.log("C wasss clickked ");
+    });
+    
+    $("a#retain").on('click', function(){
+        console.log("R wasss clicked ")
+    });
+
+});
+
+
+
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "../prospect.html");
+
+xhr.onload = function(){ 
+if (xhr.status === 200){
+    console.log('donee');
+}
+};
+xhr.send();
