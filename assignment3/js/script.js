29bb8d63410110b4cd294ae8d0b8640a6999747b
Qdesai <qaasimdesai786@gmail.com>
2022-06-11 01:53:45 -0400

    test

--- assignment3/js/script.js
+++ assignment3/js/script.js
@@ -2,14 +2,17 @@
 // Mohammed qaasim desai 
 
 $(function() {
+
     getData();
 });
 
 
 function getData(){
-    $.getJSON("data/glitchTeam.json"), function(JSONDataFromFile,satatusText,jqXHRObject){
-        $.each(JSONDataFromFile, function(i, val){
-            console.log(JSONDataFromFile);
-        });
+    $.getJSON("data/glitchTeam.json"), function(JSONDataFromFile/* ,satatusText,jqXHRObject */){
+
+        console.log(JSONDataFromFile);
+        /* $.each(JSONDataFromFile, function(i, val){
+            
+        }); */
     }
 };
