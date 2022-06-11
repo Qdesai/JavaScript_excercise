8bd7f8a5373085bc1f6e0098fc3b40d8dde2f656
Qdesai <qaasimdesai786@gmail.com>
2022-06-11 02:12:45 -0400

    test

--- assignment3/js/script.js
+++ assignment3/js/script.js
@@ -4,10 +4,11 @@
 $(function() {
 
     $.getJSON("data/glitchTeam.json"), function(JSONDataFromFile, satatusText, jqXHRObject ){
+        console.log('hiiiii');
         console.log(JSONDataFromFile);
         console.log(satatusText);
         console.log(jqXHRObject);
-        console.log('hiiiii');
+        
         /* $.each(JSONDataFromFile, function(i, val){
             
         }); */
