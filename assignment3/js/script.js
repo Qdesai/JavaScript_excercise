da0f5ff55d5d2a3dc9c6f4d613da362d8e98ccbf
Qdesai <qaasimdesai786@gmail.com>
2022-06-11 01:51:00 -0400

    created a test funciton in the script js to get the json data

--- assignment3/js/script.js
+++ assignment3/js/script.js
@@ -2,5 +2,14 @@
 // Mohammed qaasim desai 
 
 $(function() {
-       
+    getData();
 });
+
+
+function getData(){
+    $.getJSON("data/glitchTeam.json"), function(JSONDataFromFile,satatusText,jqXHRObject){
+        $.each(JSONDataFromFile, function(i, val){
+            console.log(JSONDataFromFile);
+        });
+    }
+};
