a9937fa63d152a8538cfb8b2bc1c8d8f8d603a83
Qdesai <qaasimdesai786@gmail.com>
2022-06-11 22:35:12 -0400

    test

--- assignment3/js/script.js
+++ assignment3/js/script.js
@@ -3,17 +3,18 @@
 
 $(function() {
 
-    $("a").on('click', function(){
-        ajaxDisplayTeam();
-    })        
+
+    ajaxDisplayTeam();
+    /* $("a").on('click', function(){
+        
+    })  */       
 });
 
 
 function displayTeam(){
     $.getJSON("data/glitchTeam.json", function(JSONDataFromFile, satatusText, jqXHRObject ){
         console.log(JSONDataFromFile);
-        /* console.log(satatusText);
-        console.log(jqXHRObject); */
+
         
         $.each(JSONDataFromFile.members, function(i, val){
             let loc = $(`<h4>${val.fullname}</h4>`);
@@ -27,7 +28,12 @@ function displayTeam(){
 function ajaxDisplayTeam(){
     $.ajax({
     }).get("glitchTeam.json", function(JSONDataFromFile){
-
-    }).before($("#glitch-team").html( "<h1> Loading Teams...</h1>"),
-    console.log('donee') );
+        $.each(JSONDataFromFile.members, function(i, val){
+            let loc = $(`<h4>${val.fullname}</h4>`);
+            let pos = $(`<h6>${val.position}</h6>`);
+            let shortB = $(`<p>${val.shortbio}</p><br>`);
+                $("#glitch-team").append(loc).append(pos).append(shortB);
+        });
+    })/* .before($("#glitch-team").html( "<h1> Loading Teams...</h1>"),
+    console.log('donee') ); */
     }
