1cd7c8c372195d17063fc65f8198dc039e7ffaa1
Qdesai <qaasimdesai786@gmail.com>
2022-06-11 22:40:22 -0400

    test

--- assignment3/js/script.js
+++ assignment3/js/script.js
@@ -25,13 +25,18 @@ function displayTeam(){
 };
 
 function ajaxDisplayTeam(){
-    $.ajax( $.get("glitchTeam.json", function(JSONDataFromFile){
-        $.each(JSONDataFromFile.members, function(i, val){
-            let loc = $(`<h4>${val.fullname}</h4>`);
-            let pos = $(`<h6>${val.position}</h6>`);
-            let shortB = $(`<p>${val.shortbio}</p><br>`);
-                $("#glitch-team").append(loc).append(pos).append(shortB);
-        });
-    }))/* .before($("#glitch-team").html( "<h1> Loading Teams...</h1>"),
+    $.ajax(function(){
+                            $.get("glitchTeam.json", function(JSONDataFromFile){
+                                $.each(JSONDataFromFile.members, function(i, val){
+                                    let loc = $(`<h4>${val.fullname}</h4>`);
+                                    let pos = $(`<h6>${val.position}</h6>`);
+                                    let shortB = $(`<p>${val.shortbio}</p><br>`);
+                                        $("#glitch-team").append(loc).append(pos).append(shortB);
+                                });
+                            })
+    });
+}
+
+/* .before($("#glitch-team").html( "<h1> Loading Teams...</h1>"),
     console.log('donee') ); */
     }
