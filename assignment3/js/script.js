19913e5e86ecbb0bda1e4192904927c213736700
Qdesai <qaasimdesai786@gmail.com>
2022-06-11 21:44:36 -0400

    completed the task to diaplay the json content on the page

--- assignment3/js/script.js
+++ assignment3/js/script.js
@@ -3,24 +3,23 @@
 
 $(function() {
 
-    
-
-                            $.getJSON("data/glitchTeam.json", function(JSONDataFromFile, satatusText, jqXHRObject ){
-                                console.log(JSONDataFromFile);
-                                /* console.log(satatusText);
-                                console.log(jqXHRObject); */
-                                
-                                $.each(JSONDataFromFile.members, function(i, val){
-                                    let loc = $(`<h4>${val.fullname}</h4>`);
-                                    let pos = $(`<h6>${val.position}</h6>`);
-                                    let shortB = $(`<p>${val.shortbio}</p><br>`);
-
-                                        $("#glitch-team").append(loc).append(pos).append(shortB);
-
-                                    console.log(val.fullname);
-                                    console.log(val.position);
-                                    console.log(val.shortbio);
-                                });
-                            });
+            displayTeam();
+                                        
         
 });
+
+
+function displayTeam(){
+    $.getJSON("data/glitchTeam.json", function(JSONDataFromFile, satatusText, jqXHRObject ){
+        console.log(JSONDataFromFile);
+        /* console.log(satatusText);
+        console.log(jqXHRObject); */
+        
+        $.each(JSONDataFromFile.members, function(i, val){
+            let loc = $(`<h4>${val.fullname}</h4>`);
+            let pos = $(`<h6>${val.position}</h6>`);
+            let shortB = $(`<p>${val.shortbio}</p><br>`);
+                $("#glitch-team").append(loc).append(pos).append(shortB);
+        });
+    });
+}
