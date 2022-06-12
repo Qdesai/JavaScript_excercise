b82898bb91e311c0f15962661f7cec242e572787
Qdesai <qaasimdesai786@gmail.com>
2022-06-11 20:59:57 -0400

    test

--- assignment3/js/script.js
+++ assignment3/js/script.js
@@ -11,11 +11,11 @@ $(function() {
                                 console.log(jqXHRObject); */
                                 
                                 $.each(JSONDataFromFile.members, function(i, val){
-                                        $("#glitch-team").html(`<h4>${val.fullname}</h4>
+                                        $("#glitch-team").append(`<h4>${val.fullname}</h4>
                                                                 <h6>${val.positon}</h6>
                                                                 <p>${val.shortbio}</p>
                                         `);
-                                        
+
                                     console.log(val.fullname);
                                     console.log(val.position);
                                     console.log(val.shortbio);
