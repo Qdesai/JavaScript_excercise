098c3979e454dcb5d8b2f3603bfaa5e15a7b4e9f
Qdesai <qaasimdesai786@gmail.com>
2022-08-05 18:47:04 -0400

    realised after a long time that the script should be at the bottom of the page and also made other changes

--- WEB303Test2/js/script.js
+++ WEB303Test2/js/script.js
@@ -1,22 +1,21 @@
 var form = document.getElementById('register');
+var submit = document.getElementById('submit');
+submit.disabled = false;
 var pwd = document.getElementById('p1');
 var cPwd = document.getElementById('p2');
 var chk = document.getElementById('chk');
 var dob = document.getElementById('dob');
 
 
-$(function() {
 
-    
-    $('form').submit(function(e) {
-        console.log(p1.value);
-        if(p1.value > 5){
-            console.log(p1.value);
+
+$(function() {
+        /* if(pwd.length > 5){
+            console.log(pwd.value);
         }
         else{
         e.preventDefault();
-        }
-    });
+        } */
 
 
 
