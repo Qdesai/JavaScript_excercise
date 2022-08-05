a31840c6365b81706ea37f7ba6822d27684097f0
Qdesai <qaasimdesai786@gmail.com>
2022-08-05 18:22:12 -0400

    found that jquery, css, script file was not linked so solved that problem

--- WEB303Test2/js/script.js
+++ WEB303Test2/js/script.js
@@ -1,10 +1,23 @@
-var form = document.getElementById('login');
+var form = document.getElementById('register');
+var pwd = document.getElementById('p1');
+var cPwd = document.getElementById('p2');
+var chk = document.getElementById('chk');
+var dob = document.getElementById('dob');
 
 
 $(function() {
 
+    
     $('form').submit(function(e) {
+        console.log(p1.value);
+        if(p1.value > 5){
+            console.log(p1.value);
+        }
+        else{
         e.preventDefault();
+        }
     });
+
+
 
 });
