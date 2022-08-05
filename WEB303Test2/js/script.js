f817fbcf61576ab630ef8ed4bfbceb2641c92f3f
Qdesai <qaasimdesai786@gmail.com>
2022-08-05 19:23:26 -0400

    created a half working condition of if statement for the form elements validation

--- WEB303Test2/js/script.js
+++ WEB303Test2/js/script.js
@@ -1,11 +1,19 @@
 var form = document.getElementById('register');
 var submit = document.getElementById('submit');
-submit.disabled = false;
+
+var uname = document.getElementById('username');
 var pwd = document.getElementById('p1');
 var cPwd = document.getElementById('p2');
 var chk = document.getElementById('chk');
 var dob = document.getElementById('dob');
 
+cPwd.addEventListener("keyup", function(){
+    var t = pwd.value.length;
+    if(uname.value.length >= 5 && pwd.value.length >= 12  && pwd.value === cPwd.value && chk.checked == true && dob.value){
+        console.log('hiii');
+        submit.disabled = false;
+    }
+})
 
 
 
