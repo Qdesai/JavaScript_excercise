c6fbc5220b3145d9c199854ca772a25e4161f20a
Qdesai <qaasimdesai786@gmail.com>
2022-08-06 11:13:44 -0400

    realised that the instructions said that the error should be shown when the input field is blur so changing the code

--- WEB303Test2/js/script.js
+++ WEB303Test2/js/script.js
@@ -1,32 +1,32 @@
 var form = document.getElementById('register');
 var submit = document.getElementById('submit');
-
+submit.disabled = true;
 var uname = document.getElementById('username');
 var pwd = document.getElementById('p1');
 var cPwd = document.getElementById('p2');
 var chk = document.getElementById('chk');
 var dob = document.getElementById('dob');
 
+var msg = document.getElementById('message');
 let messages = [];
 
-chk.addEventListener("click", function(){
+/* chk.addEventListener("click", function(){
     var t = pwd.value.length;
     if(uname.value.length >= 5 && pwd.value.length >= 12  && pwd.value === cPwd.value && chk.checked == true && dob.value){
         console.log('triggred');
         submit.disabled = false;
 
-        while (msg.firstChild) {
-            msg.removeChild(msg.firstChild);
-        }
+                                    while (msg.firstChild) {
+                                        msg.removeChild(msg.firstChild); // for clearing the messages from the page
+                                    }
     }
     else{
         let messages = [];
-        var msg = document.getElementById('message');
-        while (msg.firstChild) {
-            msg.removeChild(msg.firstChild);
-        }
-
-
+    
+        
+                                    while (msg.firstChild) {
+                                        msg.removeChild(msg.firstChild); // for clearing the messages from the page
+                                    }
 
         if(uname.value.length < 5 || !uname.value){
             messages.push("The UserName should contain atleast 5 charecters.");
@@ -50,9 +50,68 @@ chk.addEventListener("click", function(){
             msg.append(p);
         }
     }
+}) */
+
+
+uname.addEventListener('blur', function () {
+    
+    if(uname.value.length < 5 || !uname.value){
+        while (msg.firstChild) {
+            console.log('this was hitt');
+        }
+        messages.push("The UserName should contain atleast 5 charecters.");
+        errMsg();
+        
+    }
+    });
+
+/* pwd.addEventListener('blur', function(){
+    clrMsg();
+    if(pwd.value.length < 12){
+        messages.push("The Password must contain atleast 12 charecters.");
+        errMsg();
+    }
 })
 
+cPwd.addEventListener('blur', function(){
+    clrMsg();
+    if(pwd.value != cPwd.value){
+        messages.push("The password value does not match");
+        errMsg();
+    }
+})
 
+chk.addEventListener('blur', function(){
+    clrMsg();
+    if(chk.checked == false){
+        messages.push("The terms and condition is not accepted");
+        errMsg();
+    }
+})
+
+dob.addEventListener('blur', function(){
+    clrMsg();
+    if(!dob.value){
+        messages.push("Date of birth is not selected");
+        errMsg();
+    }
+}) */
+
+
+function errMsg(){
+    
+    for (i = 0; i< messages.length; i++){
+        var p = document.createElement('p');
+        p.append(messages[i]);
+        msg.append(p);
+    }
+}
+
+function clrMsg(){
+    while (msg.firstChild) {
+        msg.removeChild(msg.firstChild); // for clearing the messages from the page
+    }
+}
 
 $(function() {
         /* if(pwd.length > 5){
