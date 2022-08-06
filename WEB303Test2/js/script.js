716eabe6dec0111160d48d602324d02614e4b42f
Qdesai <qaasimdesai786@gmail.com>
2022-08-05 22:13:50 -0400

    some code to check the validation

--- WEB303Test2/js/script.js
+++ WEB303Test2/js/script.js
@@ -7,11 +7,31 @@ var cPwd = document.getElementById('p2');
 var chk = document.getElementById('chk');
 var dob = document.getElementById('dob');
 
+let messages = [];
+
 cPwd.addEventListener("keyup", function(){
     var t = pwd.value.length;
     if(uname.value.length >= 5 && pwd.value.length >= 12  && pwd.value === cPwd.value && chk.checked == true && dob.value){
-        console.log('hiii');
+        
         submit.disabled = false;
+    }
+    else{
+        if(uname.value.length < 5 || !uname.value){
+            messages.push("The UserName should contain atleast 5 charecters.");
+        }
+        if(pwd.value.length < 12){
+            messages.push("The Password must contain atleast 12 charecters.");
+            var 
+        }
+        if(pwd.value != cPwd.value){
+            console.log("the password value does not match");
+        }
+        if(chk.checked == false){
+            console.log('the terms and condition is not accepted');
+        }
+        if(!dob.value){
+            console.log('date of birth is not selected');
+        }
     }
 })
 
